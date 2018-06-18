d2abb9b6eb9fb246a55d16852f7ba313305fa43e
Vlad Starkovsky <vlad@starkovsky.ru>
2018-06-18 19:38:58 +0300

    18.06.2018 update

--- public_html/index.php
+++ public_html/index.php
@@ -18,8 +18,31 @@ require_once './mix.php';
 <body>
 
 <header class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
 
+            </div>
+            <div class="col-md-6">
+
+            </div>
+            <div class="col-md-2">
+
+            </div>
+            <div class="col-md-2">
+
+            </div>
+        </div>
+    </div>
 </header>
+
+
+
+
+
+<footer class="footer">
+
+</footer>
 
 
 <script src="<?php echo mix('assets/js/manifest.js') ?>" defer></script>
